4db534a7debd69e313073eb9fcd4bf32114cd25c
Ola <idebiolaoluwa@gmail.com>
2025-09-19 11:39:10 +0100

    Add docstrings to "roll_dice, player_info, save_player_data and load_player_data" functions

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ def game_state(data, filename="game_state.json"):
 
 
 def roll_dice():
-    # rols two six-sided dice and returns their values as a list
+    """rolls two six-sided dice and returns their values as a list"""
     return [randint(1, 6) for _ in range(2)]
 
 def player_info():
-    # prompts the user for their name and returns it
+    """prompts the user for their name and returns it"""
     name = input("Enter your name: ")
     piece = input("Choose your piece, 1 for Race Car, 2 for Top Hat, 3 for Scottie Dog, 4 for Cat, 5 for Penguin, 6 for Rubber duck, 7 for T-Rex and 8 for Battleship: ")
 
     return name, piece
 
 def save_player_data(player_info, filename="players.json"):
+    """Saves a player's data statues as a Json file"""
         with open(filename, "w") as f:
             json.dump(player_info, f, indent=4)
 
 def load_player_data(filename="players.json"):
+    """Loads a player's data statues from a Json file"""
         try:
             with open(filename, "r") as f:
                 return json.load(f)
